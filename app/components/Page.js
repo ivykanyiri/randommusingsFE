e1a2e92c8d8f82e6d556f9d9f0016a9dea751ed5
Ivy Kanyiri <kanyirie@gmail.com>
2023-05-24 15:34:59 +0300

    Edits

--- app/components/Page.js
+++ app/components/Page.js
@@ -1,17 +1,12 @@
-import React, {useEffect} from 'react';
-import Container from './Container';
+import React, { useEffect } from "react";
+import Container from "./Container";
 
 function Page(props) {
-
   useEffect(() => {
-    document.title = `${props.title} | React App`
-    window.scrollTo(0, 0)
-  }, [props.title])
-  return (
-    <Container wide = {props.wide}>
-      {props.children}
-    </Container>
-  )
+    document.title = `${props.title} | Random Musings`;
+    window.scrollTo(0, 0);
+  }, [props.title]);
+  return <Container wide={props.wide}>{props.children}</Container>;
 }
 
-export default Page
+export default Page;
