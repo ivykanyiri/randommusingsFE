5ed0654ca1847bf769183c1facfefd3a5ee5762e
Ivy Kanyiri <kanyirie@gmail.com>
2023-09-12 20:28:23 +0300

    Edits

--- app/components/About.js
+++ app/components/About.js
@@ -28,17 +28,13 @@ function About() {
 
   const [state, dispatch] = useImmerReducer(reducer, initialState);
 
-  const togglePassword = () => {
-    dispatch({ type: "toggleVisibility" });
-  };
+
 
   return (
     <Page title="About Us">
       <h2>About Us</h2>
       <p className="text-muted">Welcome to Random Musings, a vibrant platform where individuals can express their thoughts and experiences through personal blogging. Our website is designed to provide a space for users to share their day-to-day lives, allowing for a diverse range of stories and perspectives. Join our community and embark on a journey of self-expression as you chronicle your daily adventures and connect with like-minded individuals.</p>
 
-      <input type={state.showPassword ? "text" : "password"} onChange={(e) => dispatch({ type: "setPassword", value: e.target.value })} placeholder="enter password" />
-      <button onClick={togglePassword}>{state.showPassword ? <FaRegEyeSlash /> : <FaRegEye/> }</button>
     </Page>
   );
 }
